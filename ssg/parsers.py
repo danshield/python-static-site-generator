cfaa4a2ceb3d6302649c79f1185d2bbfb805a013
Dan Shield <Dan.Shield@allstate.com>
2020-12-23 13:20:32 -0600

    Fixing issues

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -13,12 +13,12 @@ class Parser:
         raise NotImplementedError
 
     def read(self, path: Path):
-        with open(path) as file:
+        with open(path, 'r') as file:
             return file.read()
 
     def write(self, path, dest, content, ext='.html'):
-        full_path =  self.dest / path.with_suffix(ext).name
-        with open(full_path, 'wt') as file:
+        full_path =  dest / path.with_suffix(ext).name
+        with open(full_path, 'w') as file:
             file.write(content)
 
     def copy(self, path, source, dest):
--- ssg/site.py
+++ ssg/site.py
@@ -18,7 +18,7 @@ class Site:
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
-                run_parser(path)
+                self.run_parser(path)
 
     def load_parser(self, extension):
         for parser in self.parsers:
@@ -27,8 +27,8 @@ class Site:
 
 
     def run_parser(self, path):
-        parser = load_parser(path.suffix)
+        parser = self.load_parser(path.suffix)
         if parser is not None:
-            parser.parse();
+            parser.parse(path, self.source, self.dest)
         else:
             print("Not Implemented")
