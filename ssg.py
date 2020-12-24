84b77560834c845e490f6c3c63b114ecfaf597a3
Dan Shield <Dan.Shield@allstate.com>
2020-12-24 12:52:41 -0600

    Module 4

--- ssg.py
+++ ssg.py
@@ -3,7 +3,9 @@ from ssg.site import Site
 import ssg.parsers
 
 def main(source='Content', dest='dist'):
-    config = {"source": source, "dest": dest, "parsers": [ssg.parsers.ResourceParser()]}
+    config = {"source": source, "dest": dest, "parsers": [ssg.parsers.ResourceParser(),
+                                                          ssg.parsers.MarkdownParser(),
+                                                          ssg.parsers.ReStructuredTextParser()]}
     site = Site(**config).build()
 
 
